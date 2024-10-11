c95378d13550c3d664a6640a8a24e667930fa271
Roberto Waner <waner301@gmail.com>
2024-10-11 17:29:03 -0400

    adaptaciones nuevas pero viejas ya

---
+++ WebApiForm_OPRET/WebApiForm_OPRET/Repository/FormEncuestaDbContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebApiForm_OPRET.Repository.Models;
+
+namespace WebApiForm_OPRET.Repository;
+
+public partial class FormEncuestaDbContext : DbContext
+{
+    public FormEncuestaDbContext()
+    {
+    }
+
+    public FormEncuestaDbContext(DbContextOptions<FormEncuestaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Formulario> Formularios { get; set; }
+
+    public virtual DbSet<Pregunta> Preguntas { get; set; }
+
+    public virtual DbSet<RangosRespuesta> RangosRespuestas { get; set; }
+
+    public virtual DbSet<Respuesta> Respuestas { get; set; }
+
+    public virtual DbSet<SubPregunta> SubPreguntas { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DBConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Formulario>(entity =>
+        {
+            entity.HasKey(e => e.IdentifacadorForm).HasName("PK__Formular__6CDA1CA2C6A14AE9");
+
+            entity.HasOne(d => d.IdUsuariosNavigation).WithMany(p => p.Formularios)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_User_Form");
+        });
+
+        modelBuilder.Entity<Pregunta>(entity =>
+        {
+            entity.HasKey(e => e.NoPregunta).HasName("PK__Pregunta__5DA6AFEA0903F1C5");
+
+            entity.Property(e => e.NoPregunta).ValueGeneratedNever();
+
+            entity.HasOne(d => d.IdRangoNavigation).WithMany(p => p.Preguntas).HasConstraintName("fk_Preguntas_TipRespuesta");
+
+            entity.HasOne(d => d.IdSubPreguntaNavigation).WithMany(p => p.Preguntas).HasConstraintName("fk_Preguntas_SubPreguntas");
+        });
+
+        modelBuilder.Entity<RangosRespuesta>(entity =>
+        {
+            entity.HasKey(e => e.IdRango).HasName("PK__RangosRe__A071F9CF3B29A326");
+        });
+
+        modelBuilder.Entity<Respuesta>(entity =>
+        {
+            entity.HasKey(e => e.IdRespuestas).HasName("PK__Respuest__D875135C32001BC1");
+
+            entity.HasOne(d => d.IdUsuariosNavigation).WithMany(p => p.Respuestas)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_Respuestas_User");
+
+            entity.HasOne(d => d.NoPreguntaNavigation).WithMany(p => p.Respuestas)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_Respuestas_Preguntas");
+        });
+
+        modelBuilder.Entity<SubPregunta>(entity =>
+        {
+            entity.HasKey(e => e.IdSubPregunta).HasName("PK__Sub_Preg__FF8AC1E937642627");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuarios).HasName("PK__Usuarios__854B73B310AFCBDC");
+
+            entity.Property(e => e.Estado).HasDefaultValue(true);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ WebApiForm_OPRET/WebApiForm_OPRET/Services/LoginController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApiForm_OPRET.Repository;
+using WebApiForm_OPRET.Repository.Models;
+using WebApiForm_OPRET.Services.Modelos_Tokens;
+
+namespace WebApiForm_OPRET.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly FormEncuestaDbContext _context;
+
+        public LoginController (IConfiguration config, FormEncuestaDbContext context)
+        {
+            _config = config;
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("User")]
+        public async Task<IActionResult> InicioSesion([FromBody] Login_Model login)
+        {
+            string user = login.LoginUsuario;
+            string password = login.LoginPasswords;
+
+            Usuario modelUsuario = await _context.Usuarios.FirstOrDefaultAsync(x => x.Usuario1 == user && x.Passwords == password);
+
+            if (modelUsuario == null)
+            {
+                return Unauthorized(new
+                {
+                    success = false,
+                    message = "Credenciales incorrectas",
+                    result = ""
+                });
+            }
+
+            var jwtConfig = _config.GetSection("Jwt").Get<Model_Jwt>();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, jwtConfig.Subject),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("id", modelUsuario.IdUsuarios),
+                new Claim("usuario", modelUsuario.Usuario1),
+                new Claim("cedula", modelUsuario.Cedula),
+                new Claim("email", modelUsuario.Email),
+                new Claim("rol", modelUsuario.Rol),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.Key));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+            (
+                issuer: jwtConfig.Issuer,
+                audience: jwtConfig.Audience,
+                claims: claims,
+                //expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: signIn
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new
+            {
+                success = true,
+                message = "!!!Inicio de sesion exitoso!!!",
+                result = tokenString,
+            });
+        }
+    }
+}
